ac015cbf751489d6931d8dc3cae3d06233325799
VV <vladi.vidal.l@gmail.com>
2025-11-28 00:36:06 -0300

    delete

--- SQL/verificar_estructura_tabla.sql
+++
@@ -1,9 +0,0 @@
--- =============================================
--- Verificar estructura de tabla sitios_turisticos
--- =============================================
-
--- Ver todas las columnas de la tabla
-SELECT column_name, data_type, udt_name
-FROM information_schema.columns
-WHERE table_name = 'sitios_turisticos'
-ORDER BY ordinal_position;
